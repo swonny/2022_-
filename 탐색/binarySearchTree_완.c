4fe925c2f7864dd62a333cc647a705c3229a5ebd
swonny <hanbul0921@hanmail.net>
2022-02-03 13:24:06 +0900

    22.02.03 delet_node() 루트노드 삭제 기능 수정 완료

--- 탐색/binarySearchTree_완.c
+++ 탐색/binarySearchTree_완.c
@@ -55,7 +55,6 @@ struct Node *search_node(int key)
 
 void delete_node(int key)
 {
-    // struct Node *temp = search_node(key);
     struct Node *parent = root;
     struct Node *temp = parent->left;
     struct Node *del;
@@ -81,21 +80,21 @@ void delete_node(int key)
     } else if (temp->right != NULL && temp->left == NULL) { // 왼쪽만 NULL 일 때
         parent->right = temp->right;
     } else { // 둘 다 NULL이 아닐 때
-        if(temp->right->left != NULL)  {
+        if(temp->right->left != NULL) {
             del = temp->right->left;
             while(del->left !=  NULL) { // 오른쪽 자식의 왼쪽 자식 중 외부노드 찾기
                 del = del->left;
             }
             temp->right->left = del->right;
-            del->left = temp->left;
             del->right = temp->right;
-            if(parent == root || parent->key > del->key) {
-                parent->left = del;
-            } else {
-                parent->right = del;
-            }
         } else if(temp->right->left == NULL)  {
-            parent->right = temp->right;
+            del = temp->right;
+        }
+        del->left = temp->left;
+        if(parent == root || parent->key > del->key) {
+            parent->left = del;
+        } else {
+            parent->right = del;
         }
     }
     free(temp);
@@ -124,14 +123,15 @@ int main() {
     insert_node(11);
     insert_node(6);
 
-    // delete_node(2);
+    delete_node(2);
     delete_node(3);
-    // delete_node(4);
+    delete_node(4);
     // delete_node(5);
     // delete_node(1);
     // delete_node(10);
     // delete_node(11);
-
     // printf("%d\n", root->left->key);
+    // printf("%d\n", root->left->left->key);
+    // printf("%d\n", root->left->right->key);
     print_tree(root->left);
 }
