73ac24e2c4efd1f84cd3e3bf30dce8711085cdee
최승원 <choeseung-won@choeseung-won-ui-MacBookAir.local>
2022-02-25 15:46:40 +0900

    22.02.25 my-roomchat add 참여자 목록 추가 & 삭제

--- my-roomchat/index.js
+++ my-roomchat/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const http = require('http')
+const { disconnect } = require('process')
 const socketio = require('socket.io')
 
 const port = 52273
@@ -17,17 +18,23 @@ io.on('connection', (socket) => {
 
     // 방배정 및 이름 저장
     socket.on('makeRoom', (socketInfo) => {
-        socket.join('room-' + socketInfo.roomNumber);
+        socket.join(socketInfo.roomNumber);
+        // 이름, 방번호 저장
         socket.name = socketInfo.name;
         socket.room = socketInfo.roomNumber;
-        console.log(socketInfo.room);
-        io.sockets.in('room-' + socketInfo.roomNumber).emit('connectToRoom', {
+        // 접속자 현황
+        io.sockets.in(socketInfo.roomNumber).emit('connectToRoom', {
             message : socket.name + ' now in room ' + socketInfo.roomNumber,
+            id : socket.id
         });
     })
 
+    socket.on('disconnect', (reason) => {
+        io.sockets.in(socket.room).emit('close', socket.id);
+    });
+
     socket.on('send msg', (msg) => {
-        io.emit('send msg', {
+        io.sockets.in(socket.room).emit('send msg', {
             message : msg.message
         })
     })
