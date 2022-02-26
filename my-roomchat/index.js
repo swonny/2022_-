7c71af51b9c29314b1afbe03a1150a63a77039da
최승원 <choeseung-won@choeseung-won-ui-MacBookAir.local>
2022-02-26 09:32:29 +0900

    22.02.26 my-roomchat _ 이름 불러오기 & 내 채팅과 상대 채팅 구분 기능 구현

--- my-roomchat/index.js
+++ my-roomchat/index.js
@@ -15,13 +15,16 @@ var roomNumber = '';
 
 io.on('connection', (socket) => {
     console.log('a user connected')
-
     // 방배정 및 이름 저장
     socket.on('makeRoom', (socketInfo) => {
         socket.join(socketInfo.roomNumber);
         // 이름, 방번호 저장
         socket.name = socketInfo.name;
         socket.room = socketInfo.roomNumber;
+
+        // console.log(io.of('/').adapter.rooms(1));
+        // console.log(socket)
+
         // 접속자 현황
         io.sockets.in(socketInfo.roomNumber).emit('connectToRoom', {
             message : socket.name + ' now in room ' + socketInfo.roomNumber,
@@ -35,11 +38,13 @@ io.on('connection', (socket) => {
 
     socket.on('send msg', (msg) => {
         io.sockets.in(socket.room).emit('send msg', {
-            message : msg.message
+            message : msg.message,
+            name : socket.name,
+            id : socket.id,
         })
     })
 })
 
-server.listen(port, () => {
+server.listen( port,'210.119.31.35', () => {
     console.log('Server running at http://127.0.0.1:52273')
 })
